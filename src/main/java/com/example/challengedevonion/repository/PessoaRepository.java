6553e67d794866f0c01f70e0f11e9397a04b4dc5
natan <natanaelmuller730@gmail.com>
2023-11-02 19:42:41 -0300

    Aplicado swagger no projeto!

--- src/main/java/com/example/challengedevonion/repository/PessoaRepository.java
+++ src/main/java/com/example/challengedevonion/repository/PessoaRepository.java
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
 
-    Pessoa findPessoaById(Integer id);
 }
